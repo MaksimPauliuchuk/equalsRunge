980d9e9a83b9d78de1730940327b2df56f302ae8
mpau <bortwo@gmail.com>
2016-12-16 20:34:25 +0300

    init

---
+++ src/main/java/com/pauliuchuk/equalsRunge/App.java
@@ -0,0 +1,16 @@
+package com.pauliuchuk.equalsRunge;
+
+import javax.swing.JFrame;
+
+/**
+ * Hello world!
+ */
+public class App
+{
+    public static void main(String[] args)
+    {
+        JFrame f = new JFrame("Hello World");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+}
